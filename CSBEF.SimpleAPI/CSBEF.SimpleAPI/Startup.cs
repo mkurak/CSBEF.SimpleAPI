0543d8253610d9150a0b5364bcf7ed4eea03758b
Mesut Kurak <mesutkurak@hotmail.com>
2019-10-10 04:57:43 +0300

    Dotnet Core 3 and CSBEF.Core 2.0.1 updated

--- CSBEF.SimpleAPI/CSBEF.SimpleAPI/Startup.cs
+++ CSBEF.SimpleAPI/CSBEF.SimpleAPI/Startup.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using CSBEF.Core.Concretes;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
@@ -11,43 +12,46 @@ namespace CSBEF.SimpleAPI
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public Startup(IHostingEnvironment env, IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            _configuration = configuration;
             _hostingEnvironment = env;
-            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
-            Configuration = configuration;
 
-            GlobalConfiguration.DbProvider = Configuration["AppSettings:DBSettings:Provider"];
-            GlobalConfiguration.DbConnectionString = Configuration["AppSettings:DBSettings:ConnectionString"];
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+
+            GlobalConfiguration.DbProvider = _configuration["AppSettings:DBSettings:Provider"];
+            GlobalConfiguration.DbConnectionString = _configuration["AppSettings:DBSettings:ConnectionString"];
             GlobalConfiguration.sAppPath = env.ContentRootPath;
             GlobalConfiguration.sWwwRootPath = env.WebRootPath;
         }
 
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            var serviceInit = new APIStartServiceCollection().Init(Configuration, _hostingEnvironment, services);
+            var serviceInit = new APIStartServiceCollection().Init(_configuration, _hostingEnvironment, services);
             return serviceInit;
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
-            app.UseCors(builder => builder.AllowAnyHeader().AllowCredentials().AllowAnyMethod().WithOrigins(Configuration["CustomCorsOrigin"] != null ? Configuration["CustomCorsOrigin"] : "http://localhost:8080"));
-            app.UseAuthentication();
+
+            app.UseCors(builder => builder.AllowAnyHeader().AllowCredentials().AllowAnyMethod().WithOrigins(_configuration["CustomCorsOrigin"] != null ? _configuration["CustomCorsOrigin"] : "http://localhost:8080"));
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
             loggerFactory.AddSerilog();
-            app.UseSignalR(r => r.MapSignalrRoutes());
-            app.UseMvc();
-            app.UseStaticFiles();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<GlobalHub>("/api/signalr/GlobalHub");
+            });
         }
     }
 }
