c36c43cc1533d2fb61c865dd06080fc319822fa8
Mesut Kurak <mesutkurak@hotmail.com>
2019-10-31 03:09:03 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,6 @@
-/****** Object:  Table [dbo].[UserActionLog_ActionLog]    Script Date: 10.10.2019 04:54:08 ******/
+USE [CSBEF]
+GO
+/****** Object:  Table [dbo].[UserActionLog_ActionLog]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -23,7 +25,80 @@ CREATE TABLE [dbo].[UserActionLog_ActionLog](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserManagement_Group]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Table [dbo].[UserCloudManager_FmDirectory]    Script Date: 31.10.2019 03:08:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserCloudManager_FmDirectory](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[ParentDirectoryId] [int] NULL,
+	[Title] [nvarchar](128) NOT NULL,
+	[Description] [nvarchar](512) NULL,
+	[Path] [nvarchar](4000) NOT NULL,
+	[FsKey] [uniqueidentifier] NOT NULL,
+	[Size] [bigint] NOT NULL,
+	[FileCount] [int] NOT NULL,
+	[Status] [bit] NULL,
+	[AddingDate] [datetime] NOT NULL,
+	[UpdatingDate] [datetime] NOT NULL,
+	[AddingUserId] [int] NULL,
+	[UpdatingUserId] [int] NULL,
+ CONSTRAINT [PK_UserCloudManager_FmDirectory] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserCloudManager_FmFile]    Script Date: 31.10.2019 03:08:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserCloudManager_FmFile](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[DirectoryId] [int] NOT NULL,
+	[Title] [nvarchar](128) NOT NULL,
+	[Description] [nvarchar](512) NULL,
+	[FsKey] [uniqueidentifier] NOT NULL,
+	[Path] [nvarchar](4000) NOT NULL,
+	[Size] [bigint] NOT NULL,
+	[Status] [bit] NULL,
+	[AddingDate] [datetime] NOT NULL,
+	[UpdatingDate] [datetime] NOT NULL,
+	[AddingUserId] [int] NULL,
+	[UpdatingUserId] [int] NULL,
+ CONSTRAINT [PK_UserCloudManager_FmFile] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserCloudManager_FmShare]    Script Date: 31.10.2019 03:08:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserCloudManager_FmShare](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FileId] [int] NULL,
+	[DirectoryId] [int] NULL,
+	[SharedGroupId] [int] NULL,
+	[SharedUserId] [int] NULL,
+	[Status] [bit] NULL,
+	[AddingDate] [datetime] NOT NULL,
+	[UpdatingDate] [datetime] NOT NULL,
+	[AddingUserId] [int] NULL,
+	[UpdatingUserId] [int] NULL,
+ CONSTRAINT [PK_UserCloudManager_FmShare] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserManagement_Group]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -42,7 +117,7 @@ CREATE TABLE [dbo].[UserManagement_Group](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserManagement_GroupInRole]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Table [dbo].[UserManagement_GroupInRole]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -62,7 +137,7 @@ CREATE TABLE [dbo].[UserManagement_GroupInRole](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserManagement_Role]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Table [dbo].[UserManagement_Role]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -83,7 +158,7 @@ CREATE TABLE [dbo].[UserManagement_Role](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserManagement_Token]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Table [dbo].[UserManagement_Token]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -107,7 +182,7 @@ CREATE TABLE [dbo].[UserManagement_Token](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserManagement_User]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Table [dbo].[UserManagement_User]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -133,7 +208,7 @@ CREATE TABLE [dbo].[UserManagement_User](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserManagement_UserInGroup]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Table [dbo].[UserManagement_UserInGroup]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -153,7 +228,7 @@ CREATE TABLE [dbo].[UserManagement_UserInGroup](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserManagement_UserInRole]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Table [dbo].[UserManagement_UserInRole]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -173,7 +248,7 @@ CREATE TABLE [dbo].[UserManagement_UserInRole](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserManagement_UserMessage]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Table [dbo].[UserManagement_UserMessage]    Script Date: 31.10.2019 03:08:24 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -196,13 +271,33 @@ CREATE TABLE [dbo].[UserManagement_UserMessage](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Index [IX_UserManagement_UserMessage_FromUserId]    Script Date: 10.10.2019 04:54:08 ******/
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [IX_UserCloudManager_FmDirectory]    Script Date: 31.10.2019 03:08:24 ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_UserCloudManager_FmDirectory] ON [dbo].[UserCloudManager_FmDirectory]
+(
+	[UserId] ASC,
+	[ParentDirectoryId] ASC,
+	[Title] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = ON, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [IX_UserCloudManager_FmFile]    Script Date: 31.10.2019 03:08:24 ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_UserCloudManager_FmFile] ON [dbo].[UserCloudManager_FmFile]
+(
+	[UserId] ASC,
+	[DirectoryId] ASC,
+	[Title] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = ON, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+/****** Object:  Index [IX_UserManagement_UserMessage_FromUserId]    Script Date: 31.10.2019 03:08:24 ******/
 CREATE NONCLUSTERED INDEX [IX_UserManagement_UserMessage_FromUserId] ON [dbo].[UserManagement_UserMessage]
 (
 	[FromUserId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
-/****** Object:  Index [IX_UserManagement_UserMessage_ToUserId]    Script Date: 10.10.2019 04:54:08 ******/
+/****** Object:  Index [IX_UserManagement_UserMessage_ToUserId]    Script Date: 31.10.2019 03:08:24 ******/
 CREATE NONCLUSTERED INDEX [IX_UserManagement_UserMessage_ToUserId] ON [dbo].[UserManagement_UserMessage]
 (
 	[ToUserId] ASC
@@ -215,6 +310,34 @@ GO
 ALTER TABLE [dbo].[UserActionLog_ActionLog] ADD  CONSTRAINT [DF_UserActionLog_ActionLog_AddingDate]  DEFAULT (getdate()) FOR [AddingDate]
 GO
 ALTER TABLE [dbo].[UserActionLog_ActionLog] ADD  CONSTRAINT [DF_UserActionLog_ActionLog_UpdatingDate]  DEFAULT (getdate()) FOR [UpdatingDate]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmDirectory] ADD  CONSTRAINT [DF_UserCloudManager_FmDirectory_FsKey]  DEFAULT (newid()) FOR [FsKey]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmDirectory] ADD  CONSTRAINT [DF_UserCloudManager_FmDirectory_Size]  DEFAULT ((0)) FOR [Size]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmDirectory] ADD  CONSTRAINT [DF_UserCloudManager_FmDirectory_FileCount]  DEFAULT ((0)) FOR [FileCount]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmDirectory] ADD  CONSTRAINT [DF_UserCloudManager_FmDirectory_Status]  DEFAULT ((1)) FOR [Status]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmDirectory] ADD  CONSTRAINT [DF_UserCloudManager_FmDirectory_AddingDate]  DEFAULT (getdate()) FOR [AddingDate]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmDirectory] ADD  CONSTRAINT [DF_UserCloudManager_FmDirectory_UpdatingDate]  DEFAULT (getdate()) FOR [UpdatingDate]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmFile] ADD  CONSTRAINT [DF_UserCloudManager_FmFile_FsKey]  DEFAULT (newid()) FOR [FsKey]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmFile] ADD  CONSTRAINT [DF_UserCloudManager_FmFile_Size]  DEFAULT ((0)) FOR [Size]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmFile] ADD  CONSTRAINT [DF_UserCloudManager_FmFile_Status]  DEFAULT ((1)) FOR [Status]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmFile] ADD  CONSTRAINT [DF_UserCloudManager_FmFile_AddingDate]  DEFAULT (getdate()) FOR [AddingDate]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmFile] ADD  CONSTRAINT [DF_UserCloudManager_FmFile_UpdatingDate]  DEFAULT (getdate()) FOR [UpdatingDate]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmShare] ADD  CONSTRAINT [DF_UserCloudManager_FmShare_Status]  DEFAULT ((1)) FOR [Status]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmShare] ADD  CONSTRAINT [DF_UserCloudManager_FmShare_AddingDate]  DEFAULT (getdate()) FOR [AddingDate]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmShare] ADD  CONSTRAINT [DF_UserCloudManager_FmShare_UpdatingDate]  DEFAULT (getdate()) FOR [UpdatingDate]
 GO
 ALTER TABLE [dbo].[UserManagement_Group] ADD  CONSTRAINT [DF_UserManagement_Group_Status_1]  DEFAULT ((1)) FOR [Status]
 GO
@@ -265,6 +388,21 @@ GO
 ALTER TABLE [dbo].[UserManagement_UserMessage] ADD  CONSTRAINT [DF_UserManagement_UserMessage_AddingDate_1]  DEFAULT (getdate()) FOR [AddingDate]
 GO
 ALTER TABLE [dbo].[UserManagement_UserMessage] ADD  CONSTRAINT [DF_UserManagement_UserMessage_UpdatingDate_1]  DEFAULT (getdate()) FOR [UpdatingDate]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmFile]  WITH CHECK ADD  CONSTRAINT [FK_UserCloudManager_FmFile_UserCloudManager_FmDirectory] FOREIGN KEY([DirectoryId])
+REFERENCES [dbo].[UserCloudManager_FmDirectory] ([Id])
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmFile] CHECK CONSTRAINT [FK_UserCloudManager_FmFile_UserCloudManager_FmDirectory]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmShare]  WITH CHECK ADD  CONSTRAINT [FK_UserCloudManager_FmShare_UserCloudManager_FmDirectory] FOREIGN KEY([DirectoryId])
+REFERENCES [dbo].[UserCloudManager_FmDirectory] ([Id])
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmShare] CHECK CONSTRAINT [FK_UserCloudManager_FmShare_UserCloudManager_FmDirectory]
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmShare]  WITH CHECK ADD  CONSTRAINT [FK_UserCloudManager_FmShare_UserCloudManager_FmFile] FOREIGN KEY([FileId])
+REFERENCES [dbo].[UserCloudManager_FmFile] ([Id])
+GO
+ALTER TABLE [dbo].[UserCloudManager_FmShare] CHECK CONSTRAINT [FK_UserCloudManager_FmShare_UserCloudManager_FmFile]
 GO
 ALTER TABLE [dbo].[UserManagement_GroupInRole]  WITH CHECK ADD  CONSTRAINT [FK_UserManagement_GroupInRole_UserManagement_Group] FOREIGN KEY([GroupId])
 REFERENCES [dbo].[UserManagement_Group] ([Id])
