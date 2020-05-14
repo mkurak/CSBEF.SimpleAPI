6b0360f4783df38f7f25435d72d76249dfe21e40
Mesut Kurak <mesutkurak@hotmail.com>
2020-05-14 13:04:26 +0300

    3.1.2
    - CSBEF.Core updated
    - Module libraries updated

--- Program.cs
+++ Program.cs
@@ -1,24 +1,28 @@
-using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
-namespace CSBEF.SimpleAPI {
-    public class Program {
-        public static void Main (string[] args) {
-            CreateWebHostBuilder (args).Build ().Run ();
+namespace CSBEF.SimpleAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
-            WebHost.CreateDefaultBuilder (args)
-            .UseContentRoot (Directory.GetCurrentDirectory ())
-            .ConfigureAppConfiguration ((hostingContext, config) => {
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
                 var env = hostingContext.HostingEnvironment;
-                config.AddJsonFile ("appsettings.json", optional : true, reloadOnChange : true)
-                    .AddJsonFile ($"appsettings.{env.EnvironmentName}.json",
-                        optional : true, reloadOnChange : true);
-                config.AddEnvironmentVariables ();
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
+                        optional: true, reloadOnChange: true);
+                config.AddEnvironmentVariables();
             })
-            .UseStartup<Startup> ();
+            .UseStartup<Startup>();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,3 @@
-using System;
 using CSBEF.Core.Concretes;
 using CSBEF.Core.Models;
 using Microsoft.AspNetCore.Builder;
@@ -9,17 +8,21 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 using Serilog;
+using System;
 
-namespace CSBEF.SimpleAPI {
-    public class Startup {
+namespace CSBEF.SimpleAPI
+{
+    public class Startup
+    {
         private readonly IConfiguration _configuration;
         private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public Startup (IConfiguration configuration, IWebHostEnvironment env) {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
             _configuration = configuration;
             _hostingEnvironment = env;
 
-            Log.Logger = new LoggerConfiguration ().ReadFrom.Configuration (configuration).CreateLogger ();
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
 
             GlobalConfiguration.DbProvider = _configuration["AppSettings:DBSettings:Provider"];
             GlobalConfiguration.DbConnectionString = _configuration["AppSettings:DBSettings:ConnectionString"];
@@ -27,36 +30,41 @@ namespace CSBEF.SimpleAPI {
             GlobalConfiguration.SWwwRootPath = env.WebRootPath;
         }
 
-        public IServiceProvider ConfigureServices (IServiceCollection services) {
-            var options = new ApiStartOptionsModel {
-                JsonOptionsUsing = true,
-                JsonOptionsPreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None,
-                JsonOptionsContractResolver = new CamelCasePropertyNamesContractResolver ()
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            var options = new ApiStartOptionsModel
+            {
+                JsonOptions_Using = true,
+                JsonOptions_PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None,
+                JsonOptions_ContractResolver = new CamelCasePropertyNamesContractResolver()
             };
-            var stringComparison = new StringComparison ();
-            var csbefInit = new APIStartServiceCollection ().Init (_configuration, _hostingEnvironment, services, options, stringComparison);
+            var csbefInit = new APIStartServiceCollection().Init(_configuration, _hostingEnvironment, services, options);
             return csbefInit;
         }
 
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
             }
 
-            loggerFactory.AddSerilog ();
-            app.UseStaticFiles ();
-            app.UseRouting ();
-            app.UseCors (options => {
-                options.AllowAnyHeader ();
-                options.AllowAnyMethod ();
-                options.AllowCredentials ();
-                options.WithOrigins (_configuration["CustomCorsOrigin"]);
+            loggerFactory.AddSerilog();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCors(options =>
+            {
+                options.AllowAnyHeader();
+                options.AllowAnyMethod();
+                options.AllowCredentials();
+                options.WithOrigins(_configuration["CustomCorsOrigin"]);
             });
-            app.UseAuthentication ();
-            app.UseAuthorization ();
-            app.UseEndpoints (endpoints => {
-                endpoints.MapControllers ();
-                endpoints.MapHub<GlobalHub> ("/signalr/GlobalHub");
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<GlobalHub>("/signalr/GlobalHub");
             });
         }
     }
